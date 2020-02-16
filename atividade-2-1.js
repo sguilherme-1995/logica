bcfef68cad0c58e34b41390c57794aac4af3a8d5
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-16 13:15:36 -0300

    //Comentarios

--- atividade-2-1.js
+++ atividade-2-1.js
@@ -2,21 +2,21 @@ var user = require('readline-sync');
 
 var produto = user.question("Qual o valor do produto? ");
 console.log("Agora informe quantas moedas tem em seu cofrinho!");
-var real1 = 1;
+var real1 = 1;//Aqui está o valor da moeda
 var cent50 = 0.5;
 var cent25 = 0.25;
 var cent10 = 0.10;
 var cent5 = 0.05;
 var cent1 = 0.01;
 
-var real1x = user.question("Quantas moedas de 1 Real? ");
+var real1x = user.question("Quantas moedas de 1 Real? ");//aqui entra o valor pelo qual a moeda vai ser multiplicado
 var cent50x = user.question("Quantas moedas de 50 centavos? ");
 var cent25x = user.question("Quantas moedas de 25 centavos? ");
 var cent10x = user.question("Quantas moedas de 10 centavos? ");
 var cent5x = user.question("Quantas moedas de 5 centavos? ");
 var cent1x = user.question("Quantas moedas de 1 centavos? ");
 
-var real1f = real1*real1x;
+var real1f = real1*real1x;//e aqui é o valor que vai ser computado no cofrinho
 var cent50f = cent50*cent50x;
 var cent25f = cent25*cent25x;
 var cent10f = cent10*cent10x;
