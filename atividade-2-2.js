d7e04d56b9d15035d28dfada1a4adb8759b8ea0c
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-16 13:52:53 -0300

    //Feito em casa

---
+++ atividade-2-2.js
@@ -0,0 +1,13 @@
+var user = require('readline-sync');
+var grau = user.question("Qual a medida que esta sendo usada? 1-Celsius 2-Fahrenheit ");
+var valor = user.question("Quantos graus? ");
+var cel = (valor - 32) * 5/9;
+var fah = (valor*9/5) + 32;
+
+if(grau == 1){
+    console.log("O valor em Fahrenheit e: " + fah);
+}else if(grau == 2){
+    console.log("O valor em Celsius e: " + cel);
+}else{
+    console.log("Dados incompativeis");
+}
