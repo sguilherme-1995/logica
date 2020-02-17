ba2e2a4247b1b0905cefa8f9fea785198f44a417
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-16 23:45:03 -0300

    /Feito em casa

--- atividade-1-8.js
+++ atividade-1-8.js
@@ -1,8 +1,8 @@
 var user = require('readline-sync');
 
-var tam1 = user.question("Qual a medida do primeiro lado?  ");
-var tam2 = user.question("Qual a medida do segundo lado?  ");
-var tam3 = user.question("Qual a medida do terceiro lado?  ");
+var tam1 = user.questionInt("Qual a medida do primeiro lado?  ");
+var tam2 = user.questionInt("Qual a medida do segundo lado?  ");
+var tam3 = user.questionInt("Qual a medida do terceiro lado?  ");
 
 if(tam1 == tam2 && tam2 == tam3 && tam1 == tam3){
     console.log("Triângulo Equilátero");
