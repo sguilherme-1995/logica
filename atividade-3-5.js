10d5a48c2c5090480f46e11798bce24ef285b19b
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-18 19:33:34 -0300

    while aula

--- atividade-3-5.js
+++ atividade-3-5.js
@@ -1,10 +1,10 @@
 var user = require('readline-sync');
 var num = user.questionInt("Qual o numero? ");
 var pote = user.questionInt("Qual a potencia? ");
-var soma = 0;
+var soma = num;
 
-while(pote > 0){
-    soma = soma+num;
+while(pote > 1){
+    soma = soma*num;
    
     pote--;
 }
