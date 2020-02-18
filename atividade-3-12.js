0a5ab6bfe7712711bb1b4c1719b7329fa758a206
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-18 20:18:22 -0300

    while aula

--- atividade-3-12.js
+++ atividade-3-12.js
@@ -4,7 +4,6 @@ var total = 0
     var numeb = 1;
     while(numeb <= 15){
         var nument = user.questionInt("Qual o " + numeb + " numero? ");
-        //var total = nument;
         numeb++;    
         total = total+nument;
         console.log(total)
