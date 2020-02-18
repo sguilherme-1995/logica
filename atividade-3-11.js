f7e22af80886b6e8b0972179a74f8aff8cc69a23
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-18 13:47:58 -0300

    //Feito em casa

--- atividade-3-11.js
+++ atividade-3-11.js
@@ -1,8 +1,9 @@
 var user = require('readline-sync');
 var num = user.question("Qual o numero? ");
 var tabuada = 10;
-conta = num*tabuada;
+
 while(tabuada > 0){
+    var conta = num*tabuada;
     console.log(num + " X "+tabuada+" = "+conta);
     tabuada--;
 }
