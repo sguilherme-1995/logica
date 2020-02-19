bedd2aecc3be7a5c4b26d5cd29cbba38aa7c5cfc
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-18 21:36:01 -0300

    while aula

--- atividade-3-7.js
+++ atividade-3-7.js
@@ -9,13 +9,19 @@ while(ent != "b" && ent != "B" && ent != "a" && ent != "A"){
 }
     while(ent == "b" || ent == "B"){
        
-        console.log("Realize a operação de soma!");
+        console.log("Realize a operacao de soma!");
         num1 = user.questionInt("Qual o primeiro numero? ");
         num2 = user.questionInt("Qual o segundo numero? ");
-        console.log("O resultado é: " + (num1+num2));
+        console.log("O resultado e: " + (num1+num2));
+    
         ent = user.question("Se deseja fazer outra soma digite B ou A para encerrar! ");
-        
-    }
+        while(ent != "b" && ent != "B" && ent != "a" && ent != "A"){
+            console.log("Letra invalida!");
+            ent = user.question("Insira A ou B! "); 
+       
+        }
+}
+
     console.log("Programa encerrado! ");
 
 
