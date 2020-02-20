da550b8d8c3efdd32798ed57c84e34a176c8efe3
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-20 13:10:50 -0300

    arrumado em casa

--- atividade-3-14.js
+++ atividade-3-14.js
@@ -1,6 +1,8 @@
 var user = require('readline-sync');
 var num = 1;
 var total = 0;
+var entrada = user.questionInt("Qual o "+num+" numero?  ");
+total = total+entrada;
 while(num <= 8){
     var entrada = user.questionInt("Qual o "+num+" numero?  ");
     num++;
