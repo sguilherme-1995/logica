38602974934c7a2abd30efd0f14e51da9ca47878
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-20 12:49:00 -0300

    //Feito em casa

---
+++ atividade-4-5.js
@@ -0,0 +1,18 @@
+var user = require('readline-sync');
+var num = user.questionInt("Qual o numero?  ");
+var maior = 0;
+var menor = 1;
+while(num != -1){
+    var num = user.questionInt("Qual o proximo numero?  ");
+        if(num < menor && num != -1){
+            menor = 1;
+            menor = menor + num;
+        }
+        if(num > maior && num != -1){
+            maior = 0;
+            maior = maior + num;
+        }
+
+}
+console.log("O maior numero e: "+maior);
+console.log("O menor numero e: "+menor);
