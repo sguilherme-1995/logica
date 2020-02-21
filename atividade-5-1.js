4c928000cd1ad309c2b7b2c19f9e522df0c233b1
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-21 14:17:56 -0300

    //feito em casa

--- atividade-5-1.js
+++ atividade-5-1.js
@@ -2,11 +2,11 @@ var user = require('readline-sync');
 var palavra = user.question("Digite aqui uma palavra!\n>>  ");
 var tamanho = palavra.length;
 
-for(var i=0; i<=tamanho;i++){
+for(var i=0; i<tamanho;i++){
     if(palavra.charAt(i) === "a"){
-        console.log("A");
+        console.log("> A");
     }else{
-    console.log(palavra.charAt(i));
+    console.log("> "+palavra.charAt(i));
     }
 }
 
