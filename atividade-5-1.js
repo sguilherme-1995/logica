c6cd80694d7758a1dd3072b848d07c674fc1da6b
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-02-21 14:15:47 -0300

    //Feito em casa

---
+++ atividade-5-1.js
@@ -0,0 +1,13 @@
+var user = require('readline-sync');
+var palavra = user.question("Digite aqui uma palavra!\n>>  ");
+var tamanho = palavra.length;
+
+for(var i=0; i<=tamanho;i++){
+    if(palavra.charAt(i) === "a"){
+        console.log("A");
+    }else{
+    console.log(palavra.charAt(i));
+    }
+}
+
+
