de57ac8a28b2c10ab4099fd5303a988c715e329a
Guilherme de Souza Nunes <guilhermenunes26@gmail.com>
2020-03-03 00:40:16 -0300

    //arrumado em casa

--- atividade-9-2.js
+++ atividade-9-2.js
@@ -1,18 +1,21 @@
 var user = require('readline-sync');
-var num = user.question("Qual o numero? \n");
+var num = user.questionInt("Qual o numero? \n");
 function primoFun(num){
-    var primo = 'Verdadeiro'
+    var primo = ''
     m = 2
     contador = 0
     while(m<num){
         if(num%m==0){
-            contador++
+           contador++;
         }
         if(contador>0){
-            primo = 'Falso'
+            return primo ='Falso';
         }
         m++
+        return primo = 'Verdadeiro'
+        
     }
+    
 
 }
 console.log(primoFun(num))
